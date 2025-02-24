e81efe196c721cc81acde23e98e4e3b1e801f979
王开智 <kaizhi@singtown.com>
2025-02-24 18:40:35 +0800

    Update ml library

--- mobilenet_v2/main.py
+++ mobilenet_v2/main.py
@@ -1,18 +1,22 @@
-# This code run in OpenMV4 H7 Plus
+# This code run in OpenMV H7 Plus and OpenMV RT
 
-import sensor, image, time, os, tf
+import sensor
+import time
+import ml
 
-sensor.reset()                         # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
-sensor.set_windowing((240, 240))       # Set 240x240 window.
-sensor.skip_frames(time=2000)          # Let the camera adjust.
+sensor.reset()  # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
+sensor.set_windowing((240, 240))  # Set 240x240 window.
+sensor.skip_frames(time=2000)  # Let the camera adjust.
+
+model = ml.Model("trained.tflite", load_to_fb=True)
 
 clock = time.clock()
-while(True):
+while True:
     clock.tick()
+
     img = sensor.snapshot()
-    for obj in tf.classify("trained.tflite", img, min_scale=1.0, scale_mul=0.5, x_overlap=0.0, y_overlap=0.0):
-        img.draw_rectangle(obj.rect())
-        print(obj.output())
+    result = model.predict([img])[0].flatten().tolist()
+    print("cat:", result[0], "dog", result[1])
     print(clock.fps(), "fps")
