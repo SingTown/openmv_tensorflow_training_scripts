6f0539ca396faf865e9c93d7a7202d5f93179bab
王开智 <kaizhi@singtown.com>
2025-04-21 19:41:11 +0800

    Update main.py

--- mnist/main.py
+++ mnist/main.py
@@ -1,19 +1,25 @@
-# This code run in OpenMV4 H7 or OpenMV4 H7 Plus
+# This code run in OpenMV H7, OpenMV H7 Plus and OpenMV RT
 
-import sensor, image, time, os, tf
+import sensor
+import time
+import ml
 
-sensor.reset()                         # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.GRAYSCALE)    # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
-sensor.set_windowing((240, 240))       # Set 240x240 window.
-sensor.skip_frames(time=2000)          # Let the camera adjust.
+sensor.reset()  # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.GRAYSCALE)  # Set pixel format to GRAYSCALE (or RGB565)
+sensor.set_framesize(sensor.QVGA)  # Set frame size to QVGA (320x240)
+sensor.set_windowing((240, 240))  # Set 240x240 window.
+sensor.skip_frames(time=2000)  # Let the camera adjust.
+
+model = ml.Model("trained.tflite", load_to_fb=True)
+norm = ml.Normalization(scale=(0, 1.0))
 
 clock = time.clock()
-while(True):
+while True:
     clock.tick()
-    img = sensor.snapshot().binary([(0,64)])
-    for obj in tf.classify("trained.tflite", img, min_scale=1.0, scale_mul=0.5, x_overlap=0.0, y_overlap=0.0):
-        output = obj.output()
-        number = output.index(max(output))
-        print(number)
+
+    img = sensor.snapshot().binary([(0,60)]).dilate(2)
+    input = [norm(img)] # scale 0~255 to 0~1.0
+    result = model.predict(input)[0].flatten().tolist()
+    number = result.index(max(result))
+    print("number", value)
     print(clock.fps(), "fps")
